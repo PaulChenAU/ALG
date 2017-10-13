b07ad95e3355d6e8da81e1cc03464fe62c07e09b
BoyLondon <fucshu@163.com>
2017-10-13 21:59:46 +0800

    Easy Twice submit AC

---
+++ ALG/461Hamming Distance.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# __author__=''
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        if x is None and y is None:
+            return None
+        if not x or not y:
+            return bin(y).count('1') if y else bin(x).count('1')
+        return bin(y^x).count('1')
+if __name__ == '__main__':
+    a = Solution()
+    print (a.hammingDistance(0,0))
